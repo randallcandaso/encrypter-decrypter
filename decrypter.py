f48f6f2a95d827cf5b2e4d5f4479128573730174
randallcandaso <110688328+randallcandaso@users.noreply.github.com>
2023-12-30 15:52:28 -0700

    Add files via upload

---
+++ decrypter.py
@@ -0,0 +1,101 @@
+###
+###     Author: Randall Candaso
+###     Course: CSc 110
+###     Description: This program asks the user to input an already encrypted file
+###                  along with its index file. Then, the program will take the
+###                  encrypted file and reorganize it back to its original order
+###                  based on the index file.
+###
+
+def main():
+    '''
+    This function asks the user for the encrypted file,
+    along with its index file. It will then send those
+    inputs into the insert() function.
+    '''
+    decrypt = input('Enter the name of an encrypted text file:\n')
+    index = input('Enter the name of the encryption index file:\n')
+    insert(decrypt, index)
+
+def insert(decrypt, index):
+    '''
+    This function takes the two files and organizes them
+    into new lists.
+    :param decrypt: This parameter takes from
+    the user's inputted encrypted file.
+    :param index: This parameter takes from
+    the user's inputted index file.
+    '''
+    file = open(decrypt, 'r')
+    file_2 = open(index, 'r')
+    lines = file.readlines()
+    lines_2 = file_2.readlines()
+    i = 0
+    count_2 = []
+    while i < len(lines):
+        count_2 += lines_2[i]
+        i += 1
+    list(lines, count_2)
+
+def list(count, count_2):
+    '''
+    This function takes the new lists and filters them.
+    For the second parameter, it will filter out the
+    index values that are not deemed numeric.
+    :param count: this parameter contains a list
+    of the encrypted content
+    :param count_2: this parameter contains a list
+    of the index file's content
+    '''
+    new = ''
+    new_2 = []
+    for i in (count_2):
+        new += i
+    blank = new.split('\n')
+    for i in (blank):
+        if i.isnumeric() == False:
+            blank.remove(i)
+    for i in (blank):
+        new_2.append(int(i))
+    organize(count, new_2)
+
+def organize(count, new_2):
+    '''
+    This function takes the index values and the encrypted info
+    that matches it, and organizes them into proper order.
+    :param count: this parameter contains a list
+    of the encrypted content
+    :param new_2: this parameter contains the list
+    of numeric indexes
+    '''
+    new_3 = ''
+    i = 0
+    while i < len(count):
+        minimum = min(new_2)
+        j = 0
+        z = 0
+        while z < len(new_2):
+            if new_2[z] != minimum:
+                j += 1
+            elif new_2[z] == minimum:
+                z = len(new_2)
+            z += 1
+        new_3 += count[j]
+        new_2[j] = 100
+        i += 1
+    write(new_3)
+
+def write(list):
+    '''
+    This function takes the newly created list that
+    was put into order and writes it into a new file
+    for the user.
+    :param list: this parameter pertains to the new
+    list created, which was put into order according
+    to the index values
+    '''
+    file = open('decrypted.txt', 'w')
+    file.write(str(list))
+    file.close()
+
+main()
---
+++ encrypter.py
@@ -0,0 +1,87 @@
+###
+###     Author: Randall Candaso
+###     Course: CSc 110
+###     Description: This program asks the user to input a file and in return the program
+###                  will encrypt that file. It will encrypt it by randomizing its order,
+###                  of text, along with creating a index file to serve as a key.
+###
+
+import random
+
+def main():
+    '''
+    This function prompts the user to input a file to encrypt.
+    '''
+    enter = input('Enter a name of a text file to encrypt:\n')
+    random.seed(125)
+    insert(enter)
+
+def insert(input):
+    '''
+    This function takes the inputted file and inserts the
+    info into a list. Along with that list, a second list is created
+    with matching index values.
+    :param input: this pertains to the inputted file in need of
+    decryption
+    '''
+    file = open(input, 'r')
+    line = file.readlines()
+    count = []
+    count_2 = []
+    i = 0
+    while i < len(line):
+        count.append(line[i])
+        count_2.append(i + 1)
+        i += 1
+    randomize(count, count_2)
+
+def randomize(list_1, list_2):
+    '''
+    This function randomizes the list's original order, creating the encrypted
+    effect. Along with the encrypted content of the inputted file, the indexes
+    of that content are also randomized.
+    :param list_1: This parameter contains the info originally
+    contained in the user's file
+    :param list_2: This parameter contains the matching indexes
+    for the first list.
+    '''
+    i = 0
+    limit = len(list_1) * 5
+    while i < limit:
+        number_1 = random.randint(0, len(list_1) - 1)
+        number_2 = random.randint(0, len(list_1) - 1)
+        blank = list_1[number_2]
+        list_1[number_2] = list_1[number_1]
+        list_1[number_1] = blank
+        blank_2 = list_2[number_2]
+        list_2[number_2] = list_2[number_1]
+        list_2[number_1] = blank_2
+        i += 1
+    encrypt(list_1, list_2)
+
+def encrypt(list_1, list_2):
+    '''
+    This function writes the two new lists into two new files. The first
+    file is the new encrypted file and the second is an index file
+    that can be used as a key.
+    :param list_1: this refers to the newly randomized list containing
+    the original inputted content
+    :param list_2: this list contains the randomized indexes of the
+    first list
+    '''
+    i = 0
+    file = open('encrypted.txt', 'w')
+    file.close()
+    file_2 = open('index.txt', 'w')
+    file_2.close()
+    limit = len(list_2)
+    while i < limit:
+        new_1 = open('encrypted.txt', 'a')
+        new_1.write(str(list_1[i]))
+        new_1.close()
+        new_2 = open('index.txt', 'a')
+        new_2.write(str(list_2[i]) + '\n')
+        new_2.close()
+        i += 1
+
+main()
