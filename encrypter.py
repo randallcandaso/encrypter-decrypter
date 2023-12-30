f7bb1b03f4503d89accefbd45b9ac53892a65a6c
randallcandaso <110688328+randallcandaso@users.noreply.github.com>
2023-12-30 15:53:21 -0700

    Update encrypter.py

--- encrypter.py
+++ encrypter.py
@@ -1,11 +1,3 @@
-###
-###     Author: Randall Candaso
-###     Course: CSc 110
-###     Description: This program asks the user to input a file and in return the program
-###                  will encrypt that file. It will encrypt it by randomizing its order,
-###                  of text, along with creating a index file to serve as a key.
-###
-
 import random
 
 def main():
